b7735869f86f70cd697e0930fcf57880c63484ea
Ardun Sinan Işık <141118256+Holyclaster@users.noreply.github.com>
2024-02-07 18:51:32 +0300

    Add files via upload

---
+++ C Files For VS Code/HelloWorld.C
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void funct(int x, int y) 
+{
+    int islem;
+        
+}
+
+int main()
+{
+    printf("Lutfen Yapmak Istediginiz Islemi Seciniz: \n");
+    printf("1-->'+'  2-->'-'  3-->'*'  4-->'/' \n");
+    scanf("%d %d",&x, &y);
+    switch (islem)
+    {
+    case 1:
+    printf("Islem Sonucu: %d,%d",  x+y); 
+
+        break;
+
+    case 2:
+    printf("");
+
+        break;
+
+    case 3:
+    printf("");
+
+        break;
+
+    case 4:
+    printf("");
+
+        break;
+
+    default:
+    printf("");
+        break;
+    }
+    return 0;
+}
